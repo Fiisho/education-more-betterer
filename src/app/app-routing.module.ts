3b275c61ca5ad80da91c117f83709cbcf2e7d90b
Fiisho <fiishooo@gmail.com>
2022-05-26 10:03:31 +1000

    1.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ const routes: Routes = [
     path: '', redirectTo: 'home', pathMatch: 'full' 
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
+
+
 
 ];
 @NgModule({
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public alertController: AlertController,
+  ) { }
+
+  User = '';
+  Pass = '';
 
   ngOnInit() {
+  }
+
+  async incorrectpassword() {
+    const alert = await this.alertController.create({
+      header: `Didn't Work?`,
+      subHeader: 'Wrong Password',
+      message: `Humph, It seems like you've entered the wrong password? Try again or reset it.`,
+      buttons: [
+        {
+          text: 'Try Again',
+          handler: () => {
+            console.log('Failed Password, Retrying');
+          }
+        }, {
+          text: 'Reset Password',
+          handler: () => {
+            console.log('Failed Password, Resetting Password');
+            this.router.navigate(['/forgot-password']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+  validateLogin() {
+    if (this.User === 'MCKstudent' && this.Pass === 'marist') {
+      this.router.navigate(['/topic-selection']);
+      // this.User = '';
+      // this.Pass = '';
+    } else {
+      // displays incorrect login popup
+      this.incorrectpassword();
+      this.User = '';
+      this.Pass = '';
+    }
   }
 
 }
